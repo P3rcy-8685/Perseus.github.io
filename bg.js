11df6fc18dadb9616adacf80f7d015bd244565c5
Vansh Uppal <98275514+Perseus-Jackson477@users.noreply.github.com>
2022-02-06 13:30:31 +0530

    Update bg.js

--- bg.js
+++ bg.js
@@ -18,7 +18,7 @@ function draw()
 
     ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
     ctx.fillRect(0, 0, c.width, c.height);
-    ctx.fillStyle = "rgba(0, 125, 0, 0.9)";
+    ctx.fillStyle = "rgba(0, 0, 0, 0.9)";
     ctx.font = font_size + "px arial";
     for(var i = 0; i < drops.length; i++)
     {
